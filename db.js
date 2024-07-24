14bcc380f9459503b753e7622015533bdd0536df
ChiuDave <e2243115@cmaisonneuve.qc.ca>
2024-07-23 23:57:10 -0400

    added backend config

---
+++ db.js
@@ -0,0 +1,16 @@
+const knex = require('knex');
+
+const db= knex({ // on utilise le module knex pour se connecter a la base de donnee
+    client: 'mssql', // le client sql server est mssql
+    connection: {
+      host: '127.0.0.1', // = localhost
+      user: 'admin',  // utilisateur créer
+      password: 'admin', 
+      database: 'Covoiturage', // toDo changer a nom de la database utilisé
+      options: {
+        enableArithAbort: true 
+      }
+    }
+})
+
+module.exports = {db};
