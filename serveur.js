a1a8969988cadd7cf08246aeb4f9443a5fc0eb4a
ChiuDave <e2243115@cmaisonneuve.qc.ca>
2024-08-08 19:32:58 -0400

    confirmed result connection

--- serveur.js
+++ serveur.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.get('/users', async (req,res) => {
     try {
-        db.select('*').from('your_table') // Example knex query
+        db.select('*').from('Users') // Example knex query
         .then(result => res.json(result))
         .catch(err => res.status(500).json({ error: err.message }));
     } catch(err) {
@@ -14,6 +14,15 @@ app.get('/users', async (req,res) => {
         res.status(500).json({error: 'Erreur serveur'})
     }
 })
+
+app.get('/usersTest', async (req, res) => {
+    try {
+        const result = await db ('Users').select('*'); // Example knex query
+        res.json(result);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
